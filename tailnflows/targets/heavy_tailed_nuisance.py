4bedec56daed654348f44c7e5ce10d6897fb5d8c
Lukas2412 <lukas@von-bargen.eu>
2026-08-03 15:56:43 +0000

    change sum(axis=1) to sum(dim=1)

--- tailnflows/targets/heavy_tailed_nuisance.py
+++ tailnflows/targets/heavy_tailed_nuisance.py
@@ -5,7 +5,7 @@ def log_density(x, heavy_df=1.0):
     assert x.shape[1] > 1, "Only defined for dim 2 and above"
 
     nuisance_base = torch.distributions.StudentT(df=heavy_df)
-    log_prob = nuisance_base.log_prob(x[:, :-1]).sum(axis=1)
+    log_prob = nuisance_base.log_prob(x[:, :-1]).sum(dim=1)
     log_prob += torch.distributions.Normal(loc=x[:, -2], scale=1.0).log_prob(x[:, -1])
 
     return log_prob
