a7c4c5b8cf3de74b887e82151ae97738dfe21f64
Lukas2412 <lukas@von-bargen.eu>
2026-08-06 15:45:43 +0000

    use only 140 dimensions

--- tailnflows/targets/data/__init__.py
+++ tailnflows/targets/data/__init__.py
@@ -3,7 +3,7 @@ from .fama5 import load_data as fama5_load
 from .insurance import load_data as insurance_load
 
 real_data_sources = {
-    'sp500': lambda: load_return_data(300)[0], # just the data
+    'sp500': lambda: load_return_data(140)[0], # just the data
     'fama5': fama5_load,
     'insurance': insurance_load,
 }
