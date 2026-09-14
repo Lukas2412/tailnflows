a4e68503358233ad5aa5e6da3d79d5db9e72c40d
Lukas2412 <lukas@von-bargen.eu>
2026-09-14 13:56:46 +0000

    debug mtaf and conduct big experiment

--- experiments/density_estimation_real_data/run_density_estimation.py
+++ experiments/density_estimation_real_data/run_density_estimation.py
@@ -1,16 +1,16 @@
 from pathlib import Path
 from functools import partial
+from typing import Optional
 
 import torch
 
 DEFAULT_DTYPE = torch.float32
 torch.set_default_dtype(DEFAULT_DTYPE)
 
-gpu_ix = 2
+gpu_ix = 0
 if torch.cuda.is_available():
     DEFAULT_DEVICE = torch.device(f"cuda:{gpu_ix}")
-    torch.set_default_device(DEFAULT_DEVICE) # tensors without specified device will be initialized on cuda{gpu_ix}
-    torch.cuda.set_device(DEFAULT_DEVICE) # tensors with device="cuda" will be put on cuda{gpu_ix}
+    torch.cuda.set_device(gpu_ix) # tensors with device="cuda" will be put on cuda{gpu_ix}
 else:
     DEFAULT_DEVICE = torch.device("cpu")
     torch.set_default_device("cpu")
@@ -22,27 +22,30 @@ from tailnflows.models import flows
 from tailnflows.train import data_fit
 from tailnflows.utils import add_raw_data, get_experiment_output_path, load_raw_data, get_data_path, add_experiment_output_data, parallel_runner
 from tailnflows.models.preprocessing import inverse_and_lad as t_to_norm_inverse_and_lad
-from tailnflows.models.extreme_transformations import NNKwargs, SpecifiedNNKwargs, configure_nn
+from tailnflows.models.extreme_transformations import NNKwargs
 from tailnflows.metrics import metrics
 
 """
 Model specifications
 """
 
-def base_rqs_spec(dim: int, model_config: dict) -> list[Transform]:
-    """ Builds a list of transformations representing a NSF with {depth} rational quadratic spline (rqs) layers and LU layers. """
+def base_rqs_spec(dim: int, model_config: dict, num_light: Optional[int] = None, num_heavy: Optional[int] = None) -> list[Transform]:
+    """ Builds a list of transformations representing an NSF with {depth} rational quadratic spline (rqs) layers and LU layers. """
     return flows.base_nsf_transform(
         dim, 
         num_bins=model_config.get('num_bins', 5),
         tail_bound=model_config.get('tail_bound', 3),
         affine_autoreg_layer=True,
         depth=model_config.get('depth', 1),
-        u_linear_layer=True,
+        u_linear_layer=True, # alternate rqs layers with LU layers (for >= 2 rqs layers)
         nn_kwargs=NNKwargs(
             hidden_features=model_config.get("hidden_features"),
             num_blocks=model_config.get("num_blocks"),
             use_batch_norm=model_config.get("use_batch_norm"),
         ),
+        mtaf=model_config.get('mtaf', False),
+        num_light=num_light,
+        num_heavy=num_heavy,
     )
 
 def normal(dim: int, dfs, model_config: dict) -> flows.ExperimentFlow:
@@ -52,6 +55,7 @@ def normal(dim: int, dfs, model_config: dict) -> flows.ExperimentFlow:
         use="density_estimation", 
         base_transformation_init=partial(base_rqs_spec, model_config=model_config),
         final_rotation="lu",
+        device=DEFAULT_DEVICE,
     )
 
 def get_preprocessor(dfs: list[float]): # Used for training a normal model with an initial Gaussian to Student-t transformation.
@@ -63,7 +67,7 @@ def get_preprocessor(dfs: list[float]): # Used for training a normal model with 
                 for df in dfs
             ]
         )
-        return z.to(DEFAULT_DTYPE).to(x.device), lad.to(DEFAULT_DTYPE).to(x.device)
+        return z.to(dtype=DEFAULT_DTYPE, device=x.device), lad.to(DEFAULT_DTYPE).to(x.device)
 
     return _preprocess
 
@@ -428,14 +432,18 @@ def gtaf_rqs(dim: int, metadata: dict, model_config: dict, device: torch.device 
 def mtaf_rqs(dim: int, metadata: dict, model_config: dict, device: torch.device = DEFAULT_DEVICE) -> flows.ExperimentFlow:
     """ Builds a NSF with a fixed base distribution consisting of normal distributions (for light-tailed marginals)
         and Student-t distributions with degrees of freedom given by dfs. """
+    tail_init = [df if df <= 10.0 else 0.0 for df in metadata['dfs']] # in df terms
+    num_light = int(sum(df == 0.0 for df in tail_init))
+    num_heavy = dim - num_light
+    model_config_mtaf = model_config.copy()
+    model_config_mtaf["mtaf"] = True
     return flows.build_mtaf(
         dim,
         use="density_estimation",
-        base_transformation_init=partial(base_rqs_spec, model_config=model_config),
+        base_transformation_init=partial(base_rqs_spec, model_config=model_config_mtaf, num_light=num_light, num_heavy=num_heavy),
         model_kwargs=dict(
             fix_tails=True,
-            # tail_init=metadata['dfs']
-            tail_init=[df if df <= 10.0 else 0.0 for df in metadata['dfs']] # in df terms
+            tail_init=tail_init,
         ),
         final_rotation="lu",
         device=device,
@@ -498,10 +506,13 @@ def run_experiment(
     opt_params: dict, # params for optimizer
     model_config: dict, # hyperparams for model architecture
     experiment_ix=None, # Not used
+    save_samples: bool = False,
+    save_model: bool = False,
 ):
     # general setup
     out_path = f"{data_source}/{experiment_name}"
     loss_path = f"{get_experiment_output_path()}/{out_path}/losses"
+    results_path = f"{get_experiment_output_path()}/{out_path}/results"
 
     torch.manual_seed(seed)
 
@@ -522,11 +533,8 @@ def run_experiment(
     if data_source == "climate":
         # Load train/val/test split directly as normalized tensors
         x_trn = splits_and_tail["split"]["x_trn"]
-        x_trn = x_trn.to(device=DEFAULT_DEVICE, dtype=DEFAULT_DTYPE)
         x_val = splits_and_tail["split"]["x_val"]
-        x_val = x_val.to(device=DEFAULT_DEVICE, dtype=DEFAULT_DTYPE)
         x_tst = splits_and_tail["split"]["x_tst"]
-        x_tst = x_tst.to(device=DEFAULT_DEVICE, dtype=DEFAULT_DTYPE)
         n = x_trn.shape[0] + x_val.shape[0] + x_tst.shape[0]
         print(f"Num data samps: {n}")
         dim = x_trn.shape[1]
@@ -573,41 +581,49 @@ def run_experiment(
         dim,
         metadata,
         model_config,
-    ).to(DEFAULT_DTYPE)
+    ).to(device=DEFAULT_DEVICE, dtype=DEFAULT_DTYPE)
 
     label = f'{data_source}-{split}-{model_label}'
 
     # Train and evaluate model
     fit_data = data_fit.train(
         model,
-        x_trn.to(DEFAULT_DTYPE),
-        x_val.to(DEFAULT_DTYPE),
-        x_tst.to(DEFAULT_DTYPE),
+        x_trn.to(device=DEFAULT_DEVICE, dtype=DEFAULT_DTYPE),
+        x_val.to(device=DEFAULT_DEVICE, dtype=DEFAULT_DTYPE),
+        x_tst.to(device=DEFAULT_DEVICE, dtype=DEFAULT_DTYPE),
         **opt_params,
         label=label,
         preprocess_transformation=preprocessor,
+        device=DEFAULT_DEVICE,
     )
     tst_loss, val_loss, tst_ix, losses, vlosses, steps, hook_data = fit_data
 
     # Compute metrics
-    dfs_tensor = torch.tensor(metadata["dfs"]).to(device=DEFAULT_DEVICE, dtype=DEFAULT_DTYPE)
+    dfs_tensor = torch.tensor(metadata["dfs"])
     heavy_mask = dfs_tensor > torch.zeros_like(dfs_tensor)
     if data_source == "climate": # Climate: Classify last 177 marginals as HT
         heavy_mask[-177:] = True
 
-    num_samps = x_tst.shape[0]
-    target_samps = x_tst.to(device=DEFAULT_DEVICE, dtype=DEFAULT_DTYPE)
-    synth_samps = model.sample(num_samps)
+    with torch.no_grad():
+        num_samps = x_tst.shape[0]
+        target_samps = x_tst.to(DEFAULT_DTYPE).detach().cpu()
+        synth_samps = model.sample(num_samps).detach().cpu()
 
-    sw2 = metrics.sliced_wp(synth_samps, target_samps, p=2.0)
-    w1_ht = metrics.wp_1d_mean_over_dims(synth_samps, target_samps, dims=heavy_mask, p=1.0)
-    w1_lt = metrics.wp_1d_mean_over_dims(synth_samps, target_samps, dims=~heavy_mask, p=1.0)
-    var_rel_err_ht = metrics.extreme_quantile_rel_error_multiq_over_dims(synth_samps, target_samps, heavy_mask)
-    var_rel_err_lt = metrics.extreme_quantile_rel_error_multiq_over_dims(synth_samps, target_samps, ~heavy_mask)
+        print("Compute sliced Wasserstein-2 metric ...")
+        sw2 = metrics.sliced_wp(synth_samps, target_samps, p=2.0)
+        print("Compute Wasserstein-1 metrics ...")
+        w1_ht = metrics.wp_1d_mean_over_dims(synth_samps, target_samps, dims=heavy_mask, p=1.0)
+        w1_lt = metrics.wp_1d_mean_over_dims(synth_samps, target_samps, dims=~heavy_mask, p=1.0)
+        print("Compute relative quantile errors ...")
+        var_rel_err_ht = metrics.extreme_quantile_rel_error_multiq_over_dims(synth_samps, target_samps, heavy_mask)
+        var_rel_err_lt = metrics.extreme_quantile_rel_error_multiq_over_dims(synth_samps, target_samps, ~heavy_mask)
+
 
     ################
     # Save results #
     ################
+
+    print("Save results ...")
 
     loss_ix = add_raw_data(
         loss_path,
@@ -621,39 +637,49 @@ def run_experiment(
         force_write=True,
     )
 
+    output_dict = {
+        "model": model_label,
+        "dim": dim, 
+        "seed": seed,
+        "split": split,
+        "tst_nll": float(tst_loss),
+        "val_nll": float(val_loss),
+        "sw2": sw2, # Sliced Wasserstein-2
+        "w1_ht": w1_ht, # Avg wasserstein-1 on HT dims
+        "w1_lt": w1_lt, # Avg wasserstein-1 on LT dims
+        "var99_ht": var_rel_err_ht[0].item(), # Avg rel VaR_99 difference on HT dims
+        "var995_ht": var_rel_err_ht[1].item(),
+        "var999_ht": var_rel_err_ht[2].item(),
+        "var99_lt": var_rel_err_lt[0].item(), # Avg rel VaR_99 difference on HT dims
+        "var995_lt": var_rel_err_lt[1].item(),
+        "var999_lt": var_rel_err_lt[2].item(),
+        "tst_ix": tst_ix,
+        "loss_path": loss_path,
+        "loss_ix": loss_ix,
+        **opt_params,
+        **model_config,
+    }
+
+    if save_samples:
+        output_dict["synth_samps"] = synth_samps
+
+    if save_model:
+        output_dict["model"] = model
+
     add_experiment_output_data(
-        out_path,
+        results_path,
         label,
-        {
-            "model": model_label,
-            "dim": dim, 
-            "seed": seed,
-            "split": split,
-            "tst_nll": float(tst_loss),
-            "val_nll": float(val_loss),
-            "sw2": sw2, # Sliced Wasserstein-2
-            "w1_ht": w1_ht, # Avg wasserstein-1 on HT dims
-            "w1_lt": w1_lt, # Avg wasserstein-1 on LT dims
-            "var99_ht": var_rel_err_ht[0], # Avg rel VaR_99 difference on HT dims
-            "var995_ht": var_rel_err_ht[1],
-            "var999_ht": var_rel_err_ht[2],
-            "var99_lt": var_rel_err_lt[0], # Avg rel VaR_99 difference on HT dims
-            "var995_lt": var_rel_err_lt[1],
-            "var999_lt": var_rel_err_lt[2],
-            "tst_ix": tst_ix,
-            "loss_path": loss_path,
-            "loss_ix": loss_ix,
-            **opt_params,
-            **model_config,
-        },
+        output_dict,
         force_write=True,
     )
+
+    print("Experiment completed.")
 
 
 optimisation_overrides = {
     'sp500': {
         "lr": 5e-4, 
-        "num_steps": 2_000, 
+        "num_steps": 2_000,
         "batch_size": 512, 
         "early_stop_patience": 500,
         "eval_period": 25,
@@ -669,7 +695,7 @@ optimisation_overrides = {
     },
     'insurance': {
         "lr": 5e-4, 
-        "num_steps": 2_000, 
+        "num_steps": 2_000,
         "batch_size": 512, 
         "early_stop_patience": 500,
         "eval_period": 25,
@@ -677,7 +703,7 @@ optimisation_overrides = {
     },
     'climate': {
         "lr": 1e-4, 
-        "num_steps": 20_000, 
+        "num_steps": 20_000,
         "batch_size": 512,
         "early_stop_patience": 5_00,
         "eval_period": 25,
@@ -689,29 +715,29 @@ def configured_experiments():
     """ Run several experiments in parallel by modifying the following code. """
 
     model_labels = [
-        # "normal", 
+        "normal",
         # "ttf",
-        # "ttf_hdonly",
+        "ttf_hdonly",
         # "ttf_fix", 
         # "ttf_lin",
-        # "ttf_lin_hdonly",
+        "ttf_lin_hdonly",
         # "ttf_qua",
-        # "ttf_qua_hdonly",
+        "ttf_qua_hdonly",
         # "ttf_erfi",
-        # "ttf_erfi_hdonly",
+        "ttf_erfi_hdonly",
         # "softlog",
-        # "softlog_hdonly",
+        "softlog_hdonly",
         # "softlog_lin",
-        # "softlog_lin_hdonly",
+        "softlog_lin_hdonly",
         # "arcsinh",
-        # "arcsinh_hdonly",
+        "arcsinh_hdonly",
         # "arcsinh_lin",
-        # "arcsinh_lin_hdonly",
+        "arcsinh_lin_hdonly",
         "mtaf", 
         "gtaf"
     ]
 
-    experiment_name = "2026-09-04-de-test"
+    experiment_name = "2026-09-14-de-insurance"
     # data_sources = ['climate', 'fama5', 'sp500', 'insurance']
     data_sources = ['insurance']
 
@@ -725,10 +751,8 @@ def configured_experiments():
     }
 
     # model_config
-    # depths = [1, 2]
-    depths = [1]
-    # numbers_of_bins = [5]
-    numbers_of_bins = [2] # NOTE: ONLY FOR EXPERIMENT TESTING!
+    depths = [1] # flow architecture from TTF paper
+    numbers_of_bins = [5]
     tail_bounds = [2.5] # for RQS layers, not final tail trafos!
 
     experiments = []
@@ -739,27 +763,18 @@ def configured_experiments():
 
             if data_source == "climate": # climate data uses different model config
 
-                # model_config = {
-                #     "depth": 5,
-                #     "num_bins": 3,
-                #     "tail_bound": 2.5,
-                #     "num_blocks": 2,
-                #     "hidden_features": 100,
-                #     "use_batch_norm": True,
-                # }
-                model_config = { # NOTE: ONLY FOR EXPERIMENT TESTING!
-                    "depth": 1,
-                    "num_bins": 2,
+                model_config = {
+                    "depth": 5,
+                    "num_bins": 3,
                     "tail_bound": 2.5,
-                    "num_blocks": 1,
-                    "hidden_features": 8,
-                    "use_batch_norm": False,
+                    "num_blocks": 2,
+                    "hidden_features": 100,
+                    "use_batch_norm": True,
                 }
 
                 for model_label in model_labels:
                     
-                    # opt_params = optimisation_overrides.get(data_source, opt_params)
-                    # NOTE: ONLY FOR EXPERIMENT TESTING!
+                    opt_params = optimisation_overrides.get(data_source, opt_params)
 
                     experiments.append(dict(
                         data_source=data_source,
@@ -780,12 +795,11 @@ def configured_experiments():
                                 "depth": depth,
                                 "num_bins": num_bins,
                                 "tail_bound": tail_bound,
-                            }
+                            } # hidden dimension will be 2 * feature dim
 
                             for model_label in model_labels:
                                 
-                                # opt_params = optimisation_overrides.get(data_source, opt_params)
-                                # NOTE: ONLY FOR EXPERIMENT TESTING!
+                                opt_params = optimisation_overrides.get(data_source, opt_params)
 
                                 experiments.append(dict(
                                     data_source=data_source,
@@ -797,7 +811,7 @@ def configured_experiments():
                                     model_config=model_config,
                                 ))
 
-    parallel_runner(run_experiment, experiments, max_runs=5)
+    parallel_runner(partial(run_experiment, save_samples=True), experiments, max_runs=5)
 
 if __name__ == "__main__":
     import torch.multiprocessing as mp
